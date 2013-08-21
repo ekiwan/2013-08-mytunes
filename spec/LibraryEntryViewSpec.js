6b2a9d3bc0d0800ffc3cc9959dae168f83b0386f
Hack Reactor Students <github@hackreactor.com>
2013-08-21 15:08:18 -0700

    uncomment specs

--- spec/LibraryEntryViewSpec.js
+++ spec/LibraryEntryViewSpec.js
@@ -5,7 +5,7 @@ describe('LibraryEntryView', function() {
     model = new Song({
       artist: 'Fakey McFakerson',
       title: 'Never Gonna Mock You Up',
-      url: 'example/url',
+      url: 'example/url'
     });
     // spyOn(Song.prototype, 'enqueue'); // Uncomment this when working on the second test
     spyOn(Song.prototype, 'play');
@@ -19,7 +19,7 @@ describe('LibraryEntryView', function() {
   });
 
   // Comment out the above spec when implementing the below
-  xit('queues clicked songs', function(){
+  it('queues clicked songs', function(){
     view.$el.children().first().click();
     expect(model.enqueue).toHaveBeenCalled();
   });
